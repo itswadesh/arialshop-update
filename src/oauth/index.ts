9d36eefa699f2c37dc74bb9eca2af9613dc16f94
Swadesh Behera <itswadesh@gmail.com>
2021-09-09 17:28:43 +0530

    updated oauth

--- src/oauth/index.ts
+++ src/oauth/index.ts
@@ -1,14 +1,28 @@
-import { facebookRouter, setupFacebook } from './facebook'
-import { googleRouter, setupGoogle } from './google'
-import { githubRouter, setupGithub } from './github'
-import { Router } from 'express'
 import { User } from '../models'
+import {
+  facebookRouter,
+  facebookAdminRouter,
+  setupFacebook,
+  setupFacebookAdmin,
+} from './facebook'
+import {
+  googleRouter,
+  googleAdminRouter,
+  setupGoogle,
+  setupGoogleAdmin,
+} from './google'
+import googleOneTap from './googleOneTap'
+import { Router } from 'express'
 setupFacebook(User)
+setupFacebookAdmin(User)
 setupGoogle(User)
-setupGithub(User)
+setupGoogleAdmin(User)
 
 export default function (app: Router) {
   app.use('/auth/facebook', facebookRouter)
+  app.use('/auth/facebook/admin', facebookAdminRouter)
   app.use('/auth/google', googleRouter)
-  app.use('/auth/github', githubRouter)
+  app.use('/auth/google/admin', googleAdminRouter)
+  app.use('/auth/google/onetap', googleOneTap) //not using anymore(replaced with resolver)
+  // app.get('/api/auth/google/onetap', googleOneTap)
 }
